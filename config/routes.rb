3c03596db640772ec93439ddff806fdb0208a5bb
Estera Nowak <esteranowak95@gmail.com>
2023-03-11 17:05:26 +0100

    CRUD articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  # zmieniamy na:
+
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/articles", to: "articles#index"
+  
 end
